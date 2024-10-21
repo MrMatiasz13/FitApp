aa28d9f522a8ba16950b7835b0655e42991cbf02
MrMatiasz13 <grzeskowmateusz1328@gmail.com>
2024-10-21 21:11:07 +0200

    Added checking verification of email for better securyty

--- app/src/main/java/pl/mrmatiasz/fitapp/data/repository/AuthRepositoryImpl.kt
+++ app/src/main/java/pl/mrmatiasz/fitapp/data/repository/AuthRepositoryImpl.kt
@@ -4,7 +4,6 @@ import com.google.firebase.auth.AuthResult
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.tasks.await
 import pl.mrmatiasz.fitapp.domain.repository.AuthRepository
@@ -17,10 +16,19 @@ class AuthRepositoryImpl @Inject constructor(
     override suspend fun login(email: String, password: String): Flow<Resource<AuthResult>> {
         return flow {
             emit(Resource.Loading())
-            val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
-            emit(Resource.Success(result))
-        }.catch {
-            emit(Resource.Error(it.message.toString()))
+            try {
+                val result = firebaseAuth.signInWithEmailAndPassword(email, password).await()
+
+                val user = result.user
+                if(user!!.isEmailVerified) {
+                    emit(Resource.Success(result))
+                } else {
+                    firebaseAuth.signOut()
+                    emit(Resource.Error("Email is not verified. Please verify your email before logging in."))
+                }
+            } catch (e: Exception) {
+                emit(Resource.Error(e.message ?: "An unexpected error occurred"))
+            }
         }
     }
 
@@ -31,14 +39,17 @@ class AuthRepositoryImpl @Inject constructor(
     ): Flow<Resource<AuthResult>> {
         return flow {
             emit(Resource.Loading())
-            val result = firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
-                it.user?.updateProfile(UserProfileChangeRequest.Builder().setDisplayName(username).build())
-                it.user?.sendEmailVerification()
-            }.await()
-
-            emit(Resource.Success(result))
-        }.catch {
-            emit(Resource.Error(it.message.toString()))
+            try {
+                val result = firebaseAuth.createUserWithEmailAndPassword(email, password).addOnSuccessListener {
+                    it.user!!.updateProfile(UserProfileChangeRequest.Builder()
+                        .setDisplayName(username)
+                        .build())
+                    it.user!!.sendEmailVerification()
+                }.await()
+                emit(Resource.Success(result))
+            } catch (e: Exception) {
+                emit(Resource.Error(e.message ?: "An unexpected error occurred"))
+            }
         }
     }
 }
---
+++ app/src/main/java/pl/mrmatiasz/fitapp/presentation/screens/dashboard_screen/DashboardScreen.kt
@@ -0,0 +1,15 @@
+package pl.mrmatiasz.fitapp.presentation.screens.dashboard_screen
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun DashboardScreen() {
+
+}
+
+@Preview(showBackground = true)
+@Composable
+private fun DashboardScreenPreview() {
+
+}
