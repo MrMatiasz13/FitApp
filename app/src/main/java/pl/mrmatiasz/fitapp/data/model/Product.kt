2e11ec7d75055684c5956d51e2f6e2a465a7ec0b
MrMatiasz13 <grzeskowmateusz1328@gmail.com>
2024-11-04 21:48:12 +0100

    Meal entry dropdown (complited)

---
+++ app/src/main/java/pl/mrmatiasz/fitapp/data/model/Product.kt
@@ -0,0 +1,9 @@
+package pl.mrmatiasz.fitapp.data.model
+
+data class Product(
+    val name: String,
+    val calories: Int,
+    val protein: Int,
+    val fat: Int,
+    val carbs: Int
+)
--- app/src/main/java/pl/mrmatiasz/fitapp/presentation/screens/calorie_counter_screen/CalorieCounterScreen.kt
+++ app/src/main/java/pl/mrmatiasz/fitapp/presentation/screens/calorie_counter_screen/CalorieCounterScreen.kt
@@ -1,6 +1,7 @@
 package pl.mrmatiasz.fitapp.presentation.screens.calorie_counter_screen
 
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,11 +14,14 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material.icons.filled.ArrowDropUp
+import androidx.compose.material.icons.filled.LocalFireDepartment
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
@@ -36,8 +40,27 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import pl.mrmatiasz.fitapp.data.model.Product
 import pl.mrmatiasz.fitapp.presentation.components.MacroTrackingBox
 import pl.mrmatiasz.fitapp.presentation.ui.theme.FitAppTheme
+
+val testProductList = listOf(
+    Product(
+        name = "Apple",
+        calories = 95,
+        protein = 0,
+        fat = 0,
+        carbs = 10
+    ),
+
+    Product(
+        name = "Banana",
+        calories = 105,
+        protein = 1,
+        fat = 0,
+        carbs = 15
+    )
+)
 
 @Composable
 fun CalorieCounterScreen() {
@@ -72,16 +95,14 @@ fun CalorieCounterScreen() {
                     .padding(horizontal = 8.dp)
             )
 
-            MealEntryDropdown(false)
+            MealEntryDropdown()
         }
     }
 }
 
 @Composable
-fun MealEntryDropdown (
-    isExtended: Boolean
-) {
-    var isDropDownExtended by remember { mutableStateOf(isExtended) }
+fun MealEntryDropdown() {
+    var isDropDownExtended by remember { mutableStateOf(false) }
 
     Box(
         modifier = Modifier
@@ -91,49 +112,106 @@ fun MealEntryDropdown (
                 isDropDownExtended = !isDropDownExtended
             }
     ) {
-        Row(
-            horizontalArrangement = Arrangement.SpaceBetween,
-            verticalAlignment = Alignment.CenterVertically,
+        Column(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(8.dp)
         ) {
             Row(
-                verticalAlignment = Alignment.CenterVertically
-            ) {
-                Icon(
-                    imageVector = if(!isDropDownExtended) Icons.Filled.ArrowDropUp
-                        else Icons.Filled.ArrowDropDown,
-                    contentDescription = "Arrow Drop Up/Down",
-                    modifier = Modifier
-                        .size(32.dp)
-                )
-
-                Text(
-                    text = "Breakfast",
-                    fontSize = 24.sp,
-                    fontWeight = FontWeight.SemiBold
-                )
-            }
-
-            Button(
-                onClick = { /*TODO*/ },
-                colors = ButtonDefaults.buttonColors(
-                    containerColor = MaterialTheme.colorScheme.tertiary,
-                    contentColor = Color.White
-                ),
-                border = BorderStroke(1.dp, Color.Black)
+                horizontalArrangement = Arrangement.SpaceBetween,
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier
+                    .fillMaxWidth()
             ) {
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
                     Icon(
-                        imageVector = Icons.Filled.Add,
-                        contentDescription = "Add product",
+                        imageVector = if(!isDropDownExtended) Icons.Filled.ArrowDropUp
+                        else Icons.Filled.ArrowDropDown,
+                        contentDescription = "Arrow Drop Up/Down",
+                        modifier = Modifier
+                            .size(32.dp)
                     )
 
-                    Text(text = "Add product")
+                    Text(
+                        text = "Breakfast",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.SemiBold
+                    )
                 }
+
+                Button(
+                    onClick = { /*TODO*/ },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = MaterialTheme.colorScheme.tertiary,
+                        contentColor = Color.White
+                    ),
+                    border = BorderStroke(1.dp, Color.Black)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.Add,
+                            contentDescription = "Add product",
+                        )
+
+                        Text(text = "Add product")
+                    }
+                }
+            }
+
+            if(isDropDownExtended) {
+                LazyColumn(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(8.dp)
+                ) {
+                    items(testProductList) { product ->
+                        ProductRow(product)
+                        Spacer(modifier = Modifier.height(2.dp))
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun ProductRow(product: Product) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceBetween,
+        modifier = Modifier
+            .fillMaxWidth()
+            .border(2.dp, Color.Black, RoundedCornerShape(16.dp))
+            .padding(horizontal = 16.dp, vertical = 8.dp)
+    ) {
+        Text(
+            text = product.name
+        )
+
+        Box(
+            modifier = Modifier
+                .background(Color.Gray, RoundedCornerShape(16.dp))
+                .border(2.dp, Color.Black, RoundedCornerShape(16.dp))
+                .padding(4.dp)
+        ) {
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.padding(horizontal = 4.dp)
+            ) {
+                // change to theme
+                val fireIconColor = Color(242, 125, 12)
+
+                Icon(
+                    imageVector = Icons.Filled.LocalFireDepartment,
+                    contentDescription = "Local Fire Department",
+                    tint = fireIconColor
+                )
+
+                Text(text = "${product.calories}")
             }
         }
     }
@@ -143,6 +221,6 @@ fun MealEntryDropdown (
 @Composable
 private fun CalorieCounterScreenPreview() {
     FitAppTheme(darkTheme = true, dynamicColor = false) {
-        MealEntryDropdown(false)
+        MealEntryDropdown()
     }
 }
